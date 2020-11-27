1a4fc03587796a1a62680e064e83c8018fca4e45
xyqfer <xyqfer@gmail.com>
2020-11-27 16:04:44 +0800

    update url

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/youtube/video/:id', (req, res) => {
   const trackUrl = encodeURIComponent(`https://invidious.xyz/api/v1/captions/${id}?label=English&hl=en-US`);
 
   res.render('video', {
-    src: `https://ibdkopi6vn.avosapps.us/api/v1/youtube/proxy/${id}`,
+    src: `https://v3zvmw0fii.avosapps.us/youtube/proxy/${id}`,
     track: `/proxyimage?url=${trackUrl}`,
   });
 });
