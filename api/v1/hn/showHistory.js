9a812bd20eb5a7e73f3848d434e8018b6f0b40c3
xyqfer <xyqfer@gmail.com>
2020-11-30 16:15:25 +0800

    add hn show history

---
+++ api/v1/hn/showHistory.js
@@ -0,0 +1,56 @@
+const width = 375;
+const height = 2000;
+
+function encodeUrl(fullUrl) {
+  const url = new URL(fullUrl);
+  return url.origin + url.pathname + encodeURIComponent(url.search) + encodeURIComponent(url.hash);
+}
+
+function formatUrl(url) {
+  return process.env.IMAGE_PROXY + encodeURIComponent('https://render-tron.appspot.com/screenshot/' + encodeUrl(url) + `?width=${width}&height=${height}`);
+}
+
+function getFallbackUrl(url) {
+  return process.env.LINK_PREVIEW2 + encodeUrl(url) + `?width=${width}&height=${height}`;
+}
+
+module.exports = (req, res) => {
+  const page = parseInt(req.query.page);
+  const count = 10;
+  const data = require(`${process.cwd()}/hn_show.json`);
+  let content = ''; 
+
+  for (let i = (page - 1) * count; i < page * count; i++) {
+    const item = data[i];
+    if (item) {
+      const host = (new URL(item.link)).host;
+      const preview = formatUrl(item.link);
+      const fallbackUrl = getFallbackUrl(item.link);
+      const readerViewUrl = process.env.READER_VIEW_URL + encodeURIComponent(item.link);
+
+      content += `
+        <div style="margin-bottom: 30px">
+            <a href="${item.link}" target="_blank">
+                <h4>${item.title}</h4>
+            </a>
+            <div>
+                <style>pre {width: initial !important;}</style>
+                <img src="${preview}" alt="${host}" onerror="this.onerror=null;this.src='${fallbackUrl}'" referrerpolicy="no-referrer"><br>
+                <a href="${readerViewUrl}" target="_blank">Reader View</a>
+            </div>
+        </div>
+      `;
+    }
+  }
+
+  content += `
+    <br>
+    <br>
+    <a href="./showHistory?page=${page + 1}">Next</a>
+  `;
+
+  res.render('archive', {
+      title: `Hn history - P${page}`,
+      content,
+  });
+};
--- api/v1/index.js
+++ api/v1/index.js
@@ -9,6 +9,7 @@ router.get('/hn/links', require('./hn/links'));
 router.get('/hn/story', require('./hn/story'));
 router.get('/hn/ask', require('./hn/ask'));
 router.get('/hn/askRandom', require('./hn/askRandom'));
+router.get('/hn/showHistory', require('./hn/showHistory'));
 
 const {
   hot: v2exHot,
