8f196fc194938eaaec47b8f3e413b536a5e7e239
xyqfer <xyqfer@gmail.com>
2021-01-11 14:32:59 +0800

    add code hightlight

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('x-powered-by', false);
 
+app.locals({
+  cdnHost: process.env.CDN_HOST,
+});
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
