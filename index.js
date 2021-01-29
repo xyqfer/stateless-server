a1022754c5a306c2e8587c5c0c29c187d59af7cd
xyqfer <xyqfer@gmail.com>
2021-01-29 09:20:18 +0800

    update server

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 app.set('x-powered-by', false);
 
 app.locals.cdnHost = process.env.CDN_HOST;
+app.locals.usServerHost = process.env.US_SERVER_HOST;
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
