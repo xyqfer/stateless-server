8bc7230d11d551cdead830adf06b1d330d8b930d
xyqfer <xyqfer@gmail.com>
2021-05-14 15:49:27 +0800

    update lang

--- api/v1/qiita/proxy.js
+++ api/v1/qiita/proxy.js
@@ -3,6 +3,7 @@ const { crawler } = require(`${process.cwd()}/app-libs`);
 module.exports = async (req, res) => {
   const { url } = req.query;
   const $ = await crawler(url);
+  $('html').attr('lang', 'ja-jp');
   $('script').remove();
 
   res.send($.html());
